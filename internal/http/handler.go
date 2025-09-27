0ce926379f3e2a41918258786294a16df58868bc
3keyroman <46850604+3keyroman@users.noreply.github.com>
2025-09-27 17:17:27 +0200

    Update handler.go

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/CZERTAINLY/CBOM-Repository/internal/service"
@@ -26,7 +27,12 @@ func (h Handler) Upload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	err := h.service.Upload(r.Context(), r.Body)
-	defer r.Body.Close()
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			_ = fmt.Errorf("failed to close request body: %v", err)
+		}
+	}(r.Body)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		_, _ = w.Write(fmt.Appendf(nil, "Upload failed: %q", err))
