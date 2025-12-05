16ae79d0f7caffe1d19b21474e90cf9f88579528
230385193 <230385193+devdist@users.noreply.github.com>
2025-12-05 08:41:27 +0100

    Increase code coverage for stats.

--- internal/service/stats_test.go
+++ internal/service/stats_test.go
@@ -64,6 +64,16 @@ func TestStats1(t *testing.T) {
       }
     },
     {
+      "name": "RSA-2048",
+      "type": "cryptographic-asset",
+      "bom-ref": "crypto/algorithm/rsa-2048@1.5.820.122543.8.8.8"
+    },
+    {
+      "name": "RSA",
+      "type": "framework",
+      "bom-ref": "i-made-this-up"
+    },
+    {
       "name": "AES-128-GCM",
       "type": "cryptographic-asset",
       "bom-ref": "crypto/algorithm/aes-128-gcm@2.16.840.1.101.3.4.1.6",
@@ -96,5 +106,35 @@ func TestStats1(t *testing.T) {
 	require.Equal(t, 0, bomStats.Stats.CryptoAssets.Cert.Total)
 	require.Equal(t, 0, bomStats.Stats.CryptoAssets.Protocol.Total)
 	require.Equal(t, 1, bomStats.Stats.CryptoAssets.Related.Total)
+
+}
+
+func TestStatsComponentsNil(t *testing.T) {
+	jsonBom := `{
+  "bomFormat": "CycloneDX",
+  "specVersion": "1.6",
+  "serialNumber": "urn:uuid:e8c355aa-2142-4084-a8c7-6d42c8610ba2",
+  "version": 1,
+  "metadata": {
+    "timestamp": "2024-01-09T12:00:00Z",
+    "component": {
+      "type": "application",
+      "name": "my application",
+      "version": "1.0"
+    }
+  }
+}`
+	var bom cdx.BOM
+	decoder := cdx.NewBOMDecoder(strings.NewReader(jsonBom), cdx.BOMFileFormatJSON)
+	err := decoder.Decode(&bom)
+	require.NoError(t, err)
+
+	bomStats := service.BOMStats(context.Background(), &bom)
+
+	require.Equal(t, 0, bomStats.Stats.CryptoAssets.Total)
+	require.Equal(t, 0, bomStats.Stats.CryptoAssets.Algo.Total)
+	require.Equal(t, 0, bomStats.Stats.CryptoAssets.Cert.Total)
+	require.Equal(t, 0, bomStats.Stats.CryptoAssets.Protocol.Total)
+	require.Equal(t, 0, bomStats.Stats.CryptoAssets.Related.Total)
 
 }
